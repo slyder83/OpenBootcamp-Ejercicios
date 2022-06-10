f27f85b13926bfc69925b22bb22de4be9ce35de6
slyder83 <olterg@live.com>
2022-06-10 14:33:42 +0200

    Ejercicios del tema 3 de introducción a la programación de OpenBootcamp

--- Introducción a la programación/Tema_3/src/Main.java
+++ Introducción a la programación/Tema_3/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helloooo world!");
+        int resultado;
+        resultado = suma(17, 25, 43);
+        System.out.println("El resultado de la suma es: " + resultado);
+
+        Coche miCoche = new Coche();
+        miCoche.AddPuertas();
+        System.out.println("El número de puertas es: " + miCoche.puertas);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+class Coche {
+    public int puertas = 3;
+
+    public void AddPuertas(){
+        this.puertas++;
     }
 }
