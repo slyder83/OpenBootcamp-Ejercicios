58fbed3a583570ddf10eed0179a91ab64571ba25
slyder83 <olterg@live.com>
2022-07-09 22:51:45 +0200

    Ejercicios del tema 6 del curso de python de OpenBootcamp

---
+++ Curso de Python/Tema 6/Ejercicio 1.py
@@ -0,0 +1,25 @@
+#En este ejercicio vais a crear la clase Vehículo la cual tendrá los siguientes atributos:
+#Color
+#Ruedas
+#Puertas
+#Por otro lado crearéis la clase Coche la cual heredará de Vehículo y tendrá los siguientes atributos:
+#Velocidad
+#Cilindrada
+#Por último, tendrás que crear un objeto de la clase Coche y mostrarlo por consola.
+
+class Vehiculo():
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+deportivo = Coche("verde", "4", "3", "120", "2000")
+print("Color: " + deportivo.color, ", ruedas: " + deportivo.ruedas,", puertas: " + deportivo.puertas, ", velocidad: " + deportivo.velocidad + " km/h", ", cilindrada: " + deportivo.cilindrada + " cc")
