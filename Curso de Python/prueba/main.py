73d36b5714abc8b0d2ce3c1ca7c924a58b6f04ed
slyder83 <31765498+slyder83@users.noreply.github.com>
2022-08-28 20:07:18 +0200

    Delete Curso de Python/prueba directory

--- Curso de Python/prueba/main.py
+++
@@ -1,9 +0,0 @@
-diccionario = {'Juan': 25, 'Pepe': 8, 'Alex': 89, 'Miguel': 258}
-
-def sequences_items(diccionario):
-    for i in diccionario.keys():
-        print(i)
-
-sequences_items(diccionario)
-
-
