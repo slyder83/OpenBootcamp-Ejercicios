831707cbbc5c6f78123649f92dc479b42e7bbeea
slyder83 <olterg@live.com>
2022-08-28 19:57:51 +0200

    Ejercicios del tema 1 del Curso de Java Básico de OpenBootcamp

---
+++ Curso de Python/prueba/main.py
@@ -0,0 +1,9 @@
+diccionario = {'Juan': 25, 'Pepe': 8, 'Alex': 89, 'Miguel': 258}
+
+def sequences_items(diccionario):
+    for i in diccionario.keys():
+        print(i)
+
+sequences_items(diccionario)
+
+
