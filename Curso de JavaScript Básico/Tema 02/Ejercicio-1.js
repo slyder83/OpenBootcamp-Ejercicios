6e316189bae46a158daac0f41e3889967c701bea
slyder83 <31765498+slyder83@users.noreply.github.com>
2023-01-11 16:02:47 +0100

    Ejercicios del tema 02 del Curso de JavaScript Básico de OpenBootcamp

---
+++ Curso de JavaScript Básico/Tema 02/Ejercicio-1.js
@@ -0,0 +1,14 @@
+const listaEjercicio = [
+    "Roberto",
+    39,
+    false,
+    new Date(1983, 2, 27),
+    {
+        título: "Ready Player One",
+        Autor: "Ernest Cline",
+        Fecha: new Date(2018, 0, 31),
+        Url: "https://www.amazon.es/Ready-player-one-película-Spielberg/dp/8466663061/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1673449038&sr=8-1"
+    }
+];
+
+console.log(listaEjercicio);
