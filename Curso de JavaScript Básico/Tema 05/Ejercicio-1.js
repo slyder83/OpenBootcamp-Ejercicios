05a092de0e266adbcfc8a0f807e3f05830b7ae12
slyder83 <31765498+slyder83@users.noreply.github.com>
2023-01-26 16:43:08 +0100

    Ejercicios del tema 05 del Curso de JavaScript Básico de OpenBootcamp

---
+++ Curso de JavaScript Básico/Tema 05/Ejercicio-1.js
@@ -0,0 +1,13 @@
+let alturaCm = 175;
+let alturaM = alturaCm.toFixed(2) / 100;
+let pesoKg = 72.50;
+let alturaTop = alturaM.toFixed(1);
+let pesoLow = Math.floor(pesoKg)
+let valorMax = Number.MAX_VALUE === Number.MIN_VALUE + 1;
+
+console.log(alturaCm);
+console.log(alturaM);
+console.log(pesoKg);
+console.log(alturaTop);
+console.log(pesoLow);
+console.log(valorMax);
