c731d7428f5116423a4a593712fe62d74349e4f5
slyder83 <31765498+slyder83@users.noreply.github.com>
2023-02-22 10:47:07 +0100

    Ejercicios del tema 07 del Curso de JavaScript Básico de OpenBootcamp

---
+++ Curso de JavaScript Básico/Tema 07/conjuntos.js
@@ -0,0 +1,13 @@
+const array = ["Roberto", "Manuel", "Manuela", "Samantha"];
+
+const miSet= new Set(array);
+
+console.log(miSet);
+
+miSet.add("Roberto");
+
+console.log(miSet);
+
+miSet.add("Javascript");
+
+console.log(miSet);
