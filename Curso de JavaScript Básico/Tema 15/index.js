8ca277ca9c2985d5cdf2fec9808c483b01ee8edb
slyder83 <31765498+slyder83@users.noreply.github.com>
2023-04-13 17:12:53 +0200

    Ejercicios del tema 15 del Curso de JavaScript Básico de OpenBootcamp

---
+++ Curso de JavaScript Básico/Tema 15/index.js
@@ -0,0 +1,18 @@
+const nombre = "Roberto"
+const apellido = "Ceñera"
+
+const persona = { nombre, apellido }
+
+console.log(persona);
+
+//sessionStorage.setItem("persona", JSON.stringify(persona))
+
+//localStorage.setItem("persona", JSON.stringify(persona))
+
+let tiempo = new Date()
+
+//document.cookie = "person={JSON.stringify(persona)};expires=" + new Date(tiempo.getTime()) + 2*60*1000
+
+
+
+
